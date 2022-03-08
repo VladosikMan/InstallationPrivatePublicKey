d2d26b2c42f1b9ff1c1e795d6cb5e65b746a41ce
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-08 17:03:07 +0300

    Class for generate tasks

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class Generator {
+    //class for generation variant task
+        // 1 - Id task
+        // 2 - Name cert
+        // 3 - Key data
+        // 4 - Mode ecnryption
+        // 5 - Scheme supllement
+        // 6? - Question for check knowledge student
+    private ArrayList<Task> tasks;
+    public static String createIdTask(){
+
+        return null;
+    }
+    public static String createNameCert(){
+
+        return null;
+    }
+
+    public static String createKeyData(){
+
+        return null;
+    }
+    public static Providers createProvide(){
+
+        return null;
+    }
+    public static String createQuestion(){
+
+        return null;
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
