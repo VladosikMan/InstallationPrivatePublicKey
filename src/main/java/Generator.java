5455a588997cd869a99ea74198c1b4e37d3753b5
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-10 14:49:58 +0300

    Generator : add generate questions

--- src/main/java/Generator.java
+++ src/main/java/Generator.java
@@ -1,7 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -20,10 +17,16 @@ public class Generator {
     private static ArrayList<Task> tasks = new ArrayList<>();
     private static ArrayList<String> names = new ArrayList<>();
     private static ArrayList<String> data = new ArrayList<>();
+    private static ArrayList<String> que = new ArrayList<>();
+    private static ArrayList<String> que2 = new ArrayList<>();
+    private static ArrayList<String> que3 = new ArrayList<>();
 
     public static void initialization() {
-        names = getNames();
-        data = getData();
+        names = getInformationFromFile(Path.PATH_NAMES);
+        data = getInformationFromFile(Path.PATH_DATA);
+        que = getInformationFromFile(Path.PATH_QUE_1);
+        que2 = getInformationFromFile(Path.PATH_QUE_2);
+        que3 = getInformationFromFile(Path.PATH_QUE_3);
     }
 
     public static Task generatetask() {
@@ -32,35 +35,16 @@ public class Generator {
         task.setId(createIdTask(task.getName()));
         task.setDataKey(createKeyData());
         task.setProvider(createProvide());
+        task.setQuestion(createQuestion());
         return task;
     }
 
-    private static ArrayList<String> getNames() {
-        ArrayList<String> names = new ArrayList<>();
-        try {
+    private static ArrayList<String> getInformationFromFile(String path) {
 
-            BufferedReader reader = null;
-            reader = new BufferedReader(new FileReader(Path.PATH_NAMES));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                names.add(line);
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        for (int i = 0; i < names.size(); i++)
-            System.out.println(names.get(i));
-        return names;
-    }
-
-    private static ArrayList<String> getData(){
         ArrayList<String> data = new ArrayList<>();
         try {
-
             BufferedReader reader = null;
-            reader = new BufferedReader(new FileReader(Path.PATH_DATA));
+            reader = new BufferedReader(new FileReader(path));
             String line;
             while ((line = reader.readLine()) != null) {
                 data.add(line);
@@ -70,11 +54,10 @@ public class Generator {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        for (int i = 0; i < data.size(); i++)
-            System.out.println(data.get(i));
         return data;
 
     }
+
     private static String createIdTask(String name) {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
         return name + "_" + timeStamp;
@@ -96,8 +79,8 @@ public class Generator {
     }
 
     private static String createQuestion() {
-
-        return null;
+        return que.get(0 + (int) (Math.random() * que.size())) + "\n" + que2.get(0 + (int) (Math.random() * que2.size())) +
+                "\n" + que3.get(0 + (int) (Math.random() * que3.size()));
     }
 
     private ArrayList<Task> getTasks() {
@@ -108,12 +91,14 @@ public class Generator {
         this.tasks = tasks;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException {
         Generator.initialization();
         Task task = Generator.generatetask();
-        System.out.println(task.getId());
-        System.out.println(task.getName());
-        System.out.println(task.getDataKey());
-        System.out.println(task.getProvider());
+        String que = task.getQuestion();
+        System.out.println(que);
+
+        byte bytes[] = que.getBytes("UTF-8");
+        String value = new String(bytes, "UTF-8");
+        System.out.println(value);
     }
 }
