eb5e6b535f5d6250ecb08b449747d1aae5e8f3f4
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-11 14:03:12 +0300

    Add class for save task int text file

--- src/main/java/vladgad/Storage.java
+++ src/main/java/vladgad/Storage.java
@@ -1,4 +1,41 @@
+package vladgad;
+
+import java.io.*;
+
 public class Storage {
-    //class for store
+    //class for store tasks
+    public static void saveTask(Task task){
+        String strTask = task.getId() + "\n" + task.getName() + "\n" + task.getDataKey() + "\n" +
+                task.getProvider() + "\n" + task.getQuestion();
+        System.out.println(strTask);
+        String fpath  = Path.PATH_SAVE_TASKS + task.getId() + ".txt";
+        try {
+            saveFile(fpath, strTask);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static void saveFile(String fpath, String data) throws IOException {
+//        FileOutputStream outputStream = new FileOutputStream(fpath);
+//        byte[] strToBytes = data.getBytes();
+//        outputStream.write(strToBytes);
+//        outputStream.close();
+
+        File file = new File(fpath);
+
+        // you want to output to file
+        // BufferedWriter writer = new BufferedWriter(new FileWriter(file3, true));
+        // but let's print to console while debugging
+        //BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+
+        String[] words = data.split("\n");
+        for (String word: words) {
+            writer.write(word);
+            writer.newLine();
+        }
+        writer.close();
+
+    }
 
 }
