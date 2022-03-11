caaa2a0a5b9f93eb3954b4f946936489600a8e68
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-11 15:36:39 +0300

    Storage: add function get list tasks, change delemiter task

--- src/main/java/vladgad/SendTask.java
+++
@@ -1,4 +0,0 @@
-package vladgad;
-
-public class SendTask {
-}
--- src/main/java/vladgad/Storage.java
+++ src/main/java/vladgad/Storage.java
@@ -1,41 +1,59 @@
 package vladgad;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Storage {
     //class for store tasks
-    public static void saveTask(Task task){
-        String strTask = task.getId() + "\n" + task.getName() + "\n" + task.getDataKey() + "\n" +
-                task.getProvider() + "\n" + task.getQuestion();
-        System.out.println(strTask);
-        String fpath  = Path.PATH_SAVE_TASKS + task.getId() + ".txt";
+    private static ArrayList<Task> tasks = new ArrayList<>();
+
+
+    public static void saveTask(Task task) {
+        String strTask = task.getId() + "&" + task.getName() + "&" + task.getDataKey() + "&" +
+                task.getProvider() + "&" + task.getQuestion();
+        String fpath = Path.PATH_SAVE_TASKS + task.getId() + ".txt";
         try {
             saveFile(fpath, strTask);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     private static void saveFile(String fpath, String data) throws IOException {
-//        FileOutputStream outputStream = new FileOutputStream(fpath);
-//        byte[] strToBytes = data.getBytes();
-//        outputStream.write(strToBytes);
-//        outputStream.close();
-
         File file = new File(fpath);
-
-        // you want to output to file
-        // BufferedWriter writer = new BufferedWriter(new FileWriter(file3, true));
-        // but let's print to console while debugging
-        //BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
-
         String[] words = data.split("\n");
-        for (String word: words) {
+        for (String word : words) {
             writer.write(word);
             writer.newLine();
         }
         writer.close();
+    }
 
+    public static ArrayList<Task> getTasks() {
+        for (final File fileEntry : new File(Path.PATH_SAVE_TASKS).listFiles())
+            tasks.add(readTaskFile(fileEntry));
+        return tasks;
+    }
+
+    private static Task readTaskFile(File file) {
+        Task task = new Task();
+        StringBuilder contentBuilder = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String sCurrentLine;
+            while ((sCurrentLine = br.readLine()) != null) {
+                contentBuilder.append(sCurrentLine).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String[] split = contentBuilder.toString().split("&");
+        task.setId(split[0]);
+        task.setName(split[1]);
+        task.setDataKey(split[2]);
+        task.setProvider(split[3]);
+        task.setQuestion(split[4]);
+        return task;
     }
 
 }
