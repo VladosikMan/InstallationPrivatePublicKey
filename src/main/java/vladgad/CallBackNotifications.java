58f03f48f2435f157170676781aa10661390d282
Vladislav Vedernikov <vladved2424@gmail.com>
2022-03-13 22:27:17 +0300

    Update notifications

--- src/main/java/vladgad/CallBackNotifications.java
+++ src/main/java/vladgad/CallBackNotifications.java
@@ -6,5 +6,7 @@ public enum CallBackNotifications {
     CreateCrtTask,
     CreateDataTask,
     CreateProviderTask,
-    FinishInitData
+    FinishInitData,
+    InitOpenTasks,
+    ResultCheckAnswer
 }
